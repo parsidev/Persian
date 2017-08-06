10afbd3433d8f936fd024ffab4b4a8a4121ef6c0
Mohammad Reza <parsidev@users.noreply.github.com>
2017-08-06 15:21:05 +0430

    Update Persian.php

--- src/Parsidev/Persian/Persian.php
+++ src/Parsidev/Persian/Persian.php
@@ -3,7 +3,7 @@ namespace Parsidev\Persian;
 
 class Persian
 {
-    public function arabicChar($value)
+    public static function arabicChar($value)
     {
         $arabicChars = ["ي", "ك", "‍", "دِ", "بِ", "زِ", "ذِ", "ِشِ", "ِسِ", "‌", "ى"];
         $persianChars = ["ی", "ک", "", "د", "ب", "ز", "ذ", "ش", "س", "", "ی"];
@@ -11,7 +11,7 @@ class Persian
         return str_replace($arabicChars, $persianChars, $value);
     }
 
-    public function switchKey($value){
+    public static function switchKey($value){
         $persianChar = [ "ض", "ص", "ث", "ق", "ف", "غ", "ع", "ه", "خ", "ح", "ج", "چ", "ش", "س", "ی", "ب", "ل", "ا", "ت",
             "ن", "م", "ک", "گ", "ظ", "ط", "ز", "ر", "ذ", "د", "پ", "و","؟" ];
         $englishChar = [ "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "a", "s", "d", "f", "g", "h", "j",
@@ -20,14 +20,14 @@ class Persian
         return str_replace($persianChar, $englishChar, $value);
     }
 
-    public function arabicNumber($value){
+    public static function arabicNumber($value){
         $arabicNumbers = ["١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩", "٠"];
         $persianNumbers = ["۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹", "۰"];
 
         return str_replace($arabicNumbers, $persianNumbers, $value);
     }
 
-    public function englishNumber($value){
+    public static function englishNumber($value){
         $englishNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
         $persianNumbers = ["۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹", "۰"];
 
